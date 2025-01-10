de64eb3ebf2d1ec09f03200a2ed0aca6221dd23e
Boluwatiiife <tundelawalb@gmail.com>
2025-01-10 16:13:16 +0100

    day 10 => Remove Element

---
+++ jan_10.js
@@ -0,0 +1,26 @@
+// 27. remove Element
+
+/*
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+*/
+
+const removeElement = function (nums, val) {
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+  return nums.length;
+};
+
+console.log(removeElement([3, 2, 2, 3], 3));
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+console.log(removeElement([1, 5, 6, 7, 5, 4, 4, 4, 2], 4));
