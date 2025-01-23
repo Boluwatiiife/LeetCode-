d84f9ecac5b644d0b33164f955894b5a86cc1d8b
Boluwatiiife <tundelawalb@gmail.com>
2025-01-23 16:48:55 +0100

    day 23 => Numbers of Segments in a String

---
+++ January/jan_23ii.ts
@@ -0,0 +1,29 @@
+// 434. Numbers of Segments in a String
+
+/*
+Given a string s, return the number of segments in the string.
+
+A segment is defined to be a contiguous sequence of non-space characters.
+
+ 
+
+Example 1:
+
+Input: s = "Hello, my name is John"
+Output: 5
+Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+Example 2:
+
+Input: s = "Hello"
+Output: 1
+*/
+
+const countSegments = function (s: string): any {
+  const segments = s.split(" ").filter((elem) => elem !== "");
+  return segments.length;
+};
+
+console.log(countSegments("Hello, my name is John"));
+console.log(countSegments("Hello"));
+console.log(countSegments("         "));
+console.log(countSegments(", , , ,             a, eaefa"));
