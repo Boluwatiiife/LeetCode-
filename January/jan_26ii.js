49ed624773e3123a4229b50267b58173097bebb7
Boluwatiiife <tundelawalb@gmail.com>
2025-01-26 17:49:01 +0100

    day 26 => Base 7

---
+++ January/jan_26ii.js
@@ -0,0 +1,9 @@
+// 504. Base 7
+
+const convertToBase7 = function (num) {
+  return num.toString(7);
+};
+console.log(convertToBase7(100));
+console.log(convertToBase7(-7));
+console.log(convertToBase7(33));
+console.log(convertToBase7(54));
