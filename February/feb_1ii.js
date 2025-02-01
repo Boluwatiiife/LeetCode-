46d277255debe65a5a2290a780f66f8ecb654825
Boluwatiiife <tundelawalb@gmail.com>
2025-02-01 20:47:15 +0100

    day 32 => To Lower Case

---
+++ February/feb_1ii.js
@@ -0,0 +1,9 @@
+// 709. To Lower Case
+
+const toLowerCase = function (s) {
+  return s.toLowerCase();
+};
+
+console.log(toLowerCase("Hello"));
+console.log(toLowerCase("here"));
+console.log(toLowerCase("LOVELY"));
