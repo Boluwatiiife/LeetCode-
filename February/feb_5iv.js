ab195bce34c7889848a5596abd95bfaea4621ead
Boluwatiiife <tundelawalb@gmail.com>
2025-02-05 17:06:58 +0100

    day 36 => Unique Email Addresses

---
+++ February/feb_5iv.js
@@ -0,0 +1,53 @@
+// 929. Unique Email Addresses
+
+const numUniqueEmails = function (emails) {
+  for (let i = 0; i < emails.length; i++) {
+    let [beforee, afterr] = emails[i].split("@");
+    let [frontt, backk] = beforee.split("+");
+    let arr = frontt.split("");
+    arr = arr.filter((char) => /^[a-zA-Z]$/.test(char)).join("");
+    emails[i] = arr + "@" + afterr;
+  }
+
+  let num = [...new Set(emails)];
+  return num.length;
+};
+
+console.log(
+  numUniqueEmails([
+    "test.email+alex@leetcode.com",
+    "test.e.mail+bob.cathy@leetcode.com",
+    "testemail+david@lee.tcode.com",
+  ])
+);
+console.log(
+  numUniqueEmails(["a@leetcode.com", "b@leetcode.com", "c@leetcode.com"])
+);
+console.log(
+  numUniqueEmails([
+    "fg.r.u.uzj+o.pw@kziczvh.com",
+    "r.cyo.g+d.h+b.ja@tgsg.z.com",
+    "fg.r.u.uzj+o.f.d@kziczvh.com",
+    "r.cyo.g+ng.r.iq@tgsg.z.com",
+    "fg.r.u.uzj+lp.k@kziczvh.com",
+    "r.cyo.g+n.h.e+n.g@tgsg.z.com",
+    "fg.r.u.uzj+k+p.j@kziczvh.com",
+    "fg.r.u.uzj+w.y+b@kziczvh.com",
+    "r.cyo.g+x+d.c+f.t@tgsg.z.com",
+    "r.cyo.g+x+t.y.l.i@tgsg.z.com",
+    "r.cyo.g+brxxi@tgsg.z.com",
+    "r.cyo.g+z+dr.k.u@tgsg.z.com",
+    "r.cyo.g+d+l.c.n+g@tgsg.z.com",
+    "fg.r.u.uzj+vq.o@kziczvh.com",
+    "fg.r.u.uzj+uzq@kziczvh.com",
+    "fg.r.u.uzj+mvz@kziczvh.com",
+    "fg.r.u.uzj+taj@kziczvh.com",
+    "fg.r.u.uzj+fek@kziczvh.com",
+  ])
+);
+console.log(
+  numUniqueEmails([
+    "test.email+alex@leetcode.com",
+    "test.email.leet+alex@code.com",
+  ])
+);
