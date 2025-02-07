ed43820b26ba8803d8b918b434ed62000df1c25a
Boluwatiiife <tundelawalb@gmail.com>
2025-02-07 18:35:04 +0100

    day 38 => Duplicate Zeros

---
+++ February/feb_7v.js
@@ -0,0 +1,16 @@
+// 1089. Duplicate Zeros
+
+const duplicateZeros = function (arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === 0) {
+      arr.splice(i + 1, 0, arr[i]);
+      arr.pop();
+      //   break;
+      //   continue;
+    }
+  }
+  return arr;
+};
+
+console.log(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0]));
+console.log(duplicateZeros([1, 2, 3]));
