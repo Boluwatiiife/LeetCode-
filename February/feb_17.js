f0203bb18f94566d24c11ebc6a8d91d2383dbf0f
Boluwatiiife <tundelawalb@gmail.com>
2025-02-17 13:51:30 +0100

    day 47 => Can Make Arithmetic Progression From Sequence

---
+++ February/feb_17.js
@@ -0,0 +1,15 @@
+// 1502. Can Make Arithmetic Progression From Sequence
+
+const canMakeArithmeticProgression = function (arr) {
+  arr.sort((a, b) => a - b);
+  let diff = arr[1] - arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== diff) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(canMakeArithmeticProgression([3, 5, 1]));
+console.log(canMakeArithmeticProgression([1, 2, 4]));
