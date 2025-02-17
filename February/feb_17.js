1db37c820a0b02df87ac42699608f19d9f645ce2
Boluwatiiife <tundelawalb@gmail.com>
2025-02-17 13:57:33 +0100

    day 48 => Can Make Arithmetic Progression From Sequence

--- February/feb_17.js
+++ February/feb_17.js
@@ -13,3 +13,4 @@ const canMakeArithmeticProgression = function (arr) {
 
 console.log(canMakeArithmeticProgression([3, 5, 1]));
 console.log(canMakeArithmeticProgression([1, 2, 4]));
+console.log(canMakeArithmeticProgression([1, 5, 6, 4, 3]));
