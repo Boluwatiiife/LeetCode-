8b305a0ac12788b04dd00dd2b6eaad82d00be14b
Boluwatiiife <tundelawalb@gmail.com>
2025-02-24 19:46:09 +0100

    day 55 => Can Place Flowers

--- January/jan_30.js
+++ January/jan_30.js
@@ -1,49 +1,28 @@
 // 605. Can Place Flowers
 
 const canPlaceFlowers = function (flowerbed, n) {
-  for (let i = 0; i < flowerbed.length; i + 2) {
-    if (n === 0) {
-      if (flowerbed[i] === 1 && flowerbed[i + 1] === 0) {
-        // return true;
-        i = i + 2;
-      }
-      return true;
-    }
-  }
+  let sum = 0;
 
-  let sortt = flowerbed.sort((a, b) => a - b);
-  //   return sortt;
-  for (let i = 0; i < n; i++) {
-    if (sortt[i] === 0) {
-      sortt[i] = 1;
+  for (let j = 0; j < flowerbed.length; j++) {
+    if (
+      flowerbed[j] === 0 &&
+      (j === 0 || flowerbed[j - 1] === 0) &&
+      (flowerbed.length - 1 === j || flowerbed[j + 1] === 0)
+    ) {
+      flowerbed[j] = 1;
+      sum++;
+      if (sum >= n) {
+        return true;
+      }
     }
   }
-  //   return sortt;
-  for (let i = 0; i < sortt.length; i = i + 2) {
-    // sortt[i] = "88";
-    if (sortt[i] === 0 && sortt[i + 1] === 1) {
-      [sortt[i], sortt[i + 1]] = [sortt[i + 1], sortt[i]];
-    } //   i = i + 2;
-    else if (sortt[i] === 0 && sortt[i - 1] === 1) {
-      [sortt[i], sortt[i - 1]] = [sortt[i - 1], sortt[i]];
-    } //   i = i + 2;
-    // } else if (sortt[i] === 0 && sortt[i + 1] === 1) {
-    //   //   let tempp = sortt[i];
-    //   //   sortt[i] = sortt[i + 1];
-    //   //   sortt[i + 1] = tempp;
-    //   [sortt[i], sortt[i + 1]] = [sortt[i + 1], sortt[i]];
-    // }
-  }
-  for (let i = 0; i < sortt.length; i++) {
-    if (sortt[i] === 1 && sortt[i + 1] === 1) {
-      return false;
-    }
-  }
-  return true;
-  return sortt;
+  return sum >= n;
 };
 
 console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1));
 console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2));
+console.log(canPlaceFlowers([1, 0, 0, 0, 0, 1], 2));
 console.log(canPlaceFlowers([1, 0, 0, 0, 0, 0, 1], 2));
 console.log(canPlaceFlowers([1, 0, 1, 0, 1, 0, 1], 0));
+console.log(canPlaceFlowers([0, 0, 1, 0, 1], 1));
+console.log(canPlaceFlowers([0, 0, 1, 0, 0], 1));
