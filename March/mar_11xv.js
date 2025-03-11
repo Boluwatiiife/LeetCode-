22025ab11eb2f7f3fafd99bba117e563ce1a602e
Boluwatiiife <tundelawalb@gmail.com>
2025-03-11 17:42:25 +0100

    day 70 => 2652. Sum Multiples

---
+++ March/mar_11xv.js
@@ -0,0 +1,16 @@
+// 2652. Sum Multiples
+
+const sumOfMultiples = function (n) {
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+};
+
+console.log(sumOfMultiples(7));
+console.log(sumOfMultiples(10));
+console.log(sumOfMultiples(9));
