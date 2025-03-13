c755a21f749c6bf095bf168ca18a70aef21be48b
Boluwatiiife <tundelawalb@gmail.com>
2025-03-13 09:13:46 +0100

    day 72 => 2724. Sort By

---
+++ March/mar_13vi.js
@@ -0,0 +1,18 @@
+// 2724. Sort By
+
+const sortBy = function (arr, fn) {
+  return arr.sort((a, b) => fn(a) - fn(b));
+};
+
+console.log(sortBy([5, 4, 1, 2, 3], (x) => x));
+console.log(sortBy([{ x: 1 }, { x: 0 }, { x: -1 }], (d) => d.x));
+console.log(
+  sortBy(
+    [
+      [3, 4],
+      [5, 2],
+      [10, 1],
+    ],
+    (x) => x[1]
+  )
+);
