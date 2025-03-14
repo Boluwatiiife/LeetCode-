606c4dce3533d81a3793122a57e090a37270a57b
Boluwatiiife <tundelawalb@gmail.com>
2025-03-14 06:58:56 +0100

    day 72 => 2769. Find the Maximum Achievable Number

---
+++ March/mar_14.js
@@ -0,0 +1,8 @@
+// 2769. Find the Maximum Achievable Number
+
+const theMaximumAchievableX = function (num, t) {
+  return num + t * 2;
+};
+
+console.log(theMaximumAchievableX(4, 1));
+console.log(theMaximumAchievableX(3, 2));
