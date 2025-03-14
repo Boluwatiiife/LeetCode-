90417c0259a03794dce0c3af2a077092e2f83253
Boluwatiiife <tundelawalb@gmail.com>
2025-03-14 15:32:47 +0100

    day 73 => 2810. Faulty Keyboard

---
+++ March/mar_14viii.js
@@ -0,0 +1,18 @@
+// 2810. Faulty Keyboard
+
+const finalString = function (s) {
+  let arr = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "i") {
+      arr.reverse();
+    } else {
+      arr.push(s[i]);
+    }
+  }
+
+  return arr.join("");
+};
+
+console.log(finalString("string"));
+console.log(finalString("poiinter"));
