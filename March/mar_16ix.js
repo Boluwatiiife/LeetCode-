b667132235bea03d127b209ead98ea3772c5ddc9
Boluwatiiife <tundelawalb@gmail.com>
2025-03-16 19:19:45 +0100

    day 75 => 2951. Find the Peaks

---
+++ March/mar_16ix.js
@@ -0,0 +1,15 @@
+// 2951. Find the Peaks
+
+const findPeaks = function (mountain) {
+  let arr = [];
+
+  for (let i = 1; i < mountain.length - 1; i++) {
+    if (mountain[i] > mountain[i + 1] && mountain[i] > mountain[i - 1]) {
+      arr.push(i);
+    }
+  }
+  return arr;
+};
+
+console.log(findPeaks([2, 4, 4]));
+console.log(findPeaks([1, 4, 3, 8, 5]));
