9a019d7219d7b0a6c9459e9dae3dc06434003a87
Boluwatiiife <tundelawalb@gmail.com>
2025-03-17 07:19:49 +0100

    day 76 => 2974. Minimum Number Game

---
+++ March/mar_17ii.js
@@ -0,0 +1,16 @@
+// 2974. Minimum Number Game
+
+const numberGame = function (nums) {
+  nums = nums.sort((a, b) => a - b);
+  let arr = [];
+
+  for (let i = 0; i < nums.length; i += 2) {
+    arr.push(nums[i + 1]);
+    arr.push(nums[i]);
+  }
+
+  return arr;
+};
+
+console.log(numberGame([5, 4, 2, 3]));
+console.log(numberGame([2, 5]));
