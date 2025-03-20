03d5a5408d3ab3d7ea30bb5d19007f4cdf8236e2
Boluwatiiife <tundelawalb@gmail.com>
2025-03-20 20:24:25 +0100

    day 79 => 3226. Number of Bit Changes to Make Two Integers Equal

---
+++ March/mar_20xv.js
@@ -0,0 +1,43 @@
+// 3226. Number of Bit Changes to Make Two Integers Equal
+
+const minChanges = function (n, k) {
+  let one = n.toString(2);
+  let two = k.toString(2);
+  let maxx = Math.max(one.length, two.length);
+  let nn = Math.abs(one.length - two.length);
+
+  if (one.length !== maxx) {
+    for (let i = 0; i < nn; i++) {
+      one = "0" + one;
+    }
+  }
+  if (two.length !== maxx) {
+    for (let i = 0; i < nn; i++) {
+      two = "0" + two;
+    }
+  }
+
+  let onee = one.split("");
+  let twoo = two.split("");
+  let count = 0;
+
+  for (let i = 0; i < onee.length; i++) {
+    if (onee[i] === "1" && twoo[i] === "0") {
+      onee[i] = "0";
+      count++;
+    }
+  }
+  onee = onee.join("");
+  twoo = twoo.join("");
+
+  //   return count;
+  return onee === twoo ? count : -1;
+  //   return nn;
+  return [one, two];
+
+  return [one.length, two.length];
+};
+
+console.log(minChanges(13, 4));
+console.log(minChanges(21, 21));
+console.log(minChanges(14, 13));
