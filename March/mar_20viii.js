907b8a3fa80113ff0f1711425315ae90e14d3d93
Boluwatiiife <tundelawalb@gmail.com>
2025-03-20 12:24:15 +0100

    day 79 => 3190. Find Minimum Operations to Make All Elements Divisible by Three

---
+++ March/mar_20viii.js
@@ -0,0 +1,15 @@
+// 3190. Find Minimum Operations to Make All Elements Divisible by Three
+
+const minimumOperations = function (nums) {
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 3 !== 0) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(minimumOperations([1, 2, 3, 4]));
+console.log(minimumOperations([3, 6, 9]));
