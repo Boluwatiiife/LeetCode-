2c282c2fdeb46ffa82347499ac60a263c0dcf976
Boluwatiiife <tundelawalb@gmail.com>
2025-03-22 07:32:13 +0100

    day 80 => 3360. Stone Removal Game

---
+++ March/mar_22.js
@@ -0,0 +1,18 @@
+// 3360. Stone Removal Game
+
+const canAliceWin = function (n) {
+  let num = n;
+  let ten = 10;
+  let sum = 0;
+  while (true) {
+    if (ten > num) break;
+    num -= ten;
+    ten--;
+    sum++;
+  }
+  return sum % 2 !== 0 ? true : false;
+};
+
+console.log(canAliceWin(12));
+console.log(canAliceWin(1));
+console.log(canAliceWin(27));
