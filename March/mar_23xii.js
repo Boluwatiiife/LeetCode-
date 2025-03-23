fced94522818f4d8495e3f6ae45fb0f9605a7217
Boluwatiiife <tundelawalb@gmail.com>
2025-03-23 19:06:26 +0100

    day 82 => return count > 1 ? maxx : Math.max(...nums)

---
+++ March/mar_23xii.js
@@ -0,0 +1,27 @@
+// 3471. Find the Largest Almost Missing Integer
+
+const largestInteger = function (nums, k) {
+  let arr = new Map();
+  let count = 0;
+
+  for (let i = 0; i < nums.length - k + 1; i++) {
+    let temp = nums.slice(i, k + i);
+    count++;
+    for (no of temp) {
+      arr.set(no, (arr.get(no) || 0) + 1);
+    }
+  }
+  let maxx = -1;
+  for (no of nums) {
+    if (arr.get(no) === 1) {
+      maxx = Math.max(maxx, no);
+    }
+  }
+  return count > 1 ? maxx : Math.max(...nums);
+};
+
+console.log(largestInteger([3, 9, 2, 1, 7], 3));
+console.log(largestInteger([3, 9, 7, 2, 1, 7], 4));
+console.log(largestInteger([0, 0], 1));
+console.log(largestInteger([0, 0], 2));
+console.log(largestInteger([0, 50], 2));
