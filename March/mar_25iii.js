2e51df04f4425e56d93a8e67b78054160fff1693
Boluwatiiife <tundelawalb@gmail.com>
2025-03-25 10:28:28 +0100

    day 84 => 15. 3Sum

---
+++ March/mar_25iii.js
@@ -0,0 +1,34 @@
+// 15. 3Sum
+
+const threeSum = function (nums) {
+  nums = nums.sort((a, b) => a - b);
+  let arr = [];
+  let n = nums.length;
+  for (let i = 0; i < n - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1,
+      right = n - 1;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        arr.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+console.log(threeSum([0, 1, 1]));
+console.log(threeSum([0, 0, 0]));
+console.log(threeSum([0, 0, 0, 0]));
