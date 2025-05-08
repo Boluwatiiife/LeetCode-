41880fe0118706a7108fac2674f56fbeffa3bd38
Boluwatiiife <tundelawalb@gmail.com>
2025-05-08 08:31:00 +0100

    day 128 => 524. Longest Word in Dictionary through Deleting

---
+++ May/may_8ii.js
@@ -0,0 +1,23 @@
+// 524. Longest Word in Dictionary through Deleting
+
+const findLongestWord = function (s, dictionary) {
+  function check_sub(a, b) {
+    let i = 0;
+    j = 0;
+    while (i < a.length && j < b.length) {
+      if (a[i] === b[j]) i++;
+      j++;
+    }
+    return i === a.length;
+  }
+  dictionary.sort();
+  dictionary.sort((a, b) => b.length - a.length);
+
+  for (let word of dictionary) {
+    if (check_sub(word, s)) return word;
+  }
+  return "";
+};
+
+console.log(findLongestWord("abpcplea", ["ale", "apple", "monkey", "plea"]));
+console.log(findLongestWord("abpcplea", ["a", "b", "c"]));
