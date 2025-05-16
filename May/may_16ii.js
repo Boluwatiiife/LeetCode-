226a6914dae1325648927f7dc484ef21f9746b6c
Boluwatiiife <tundelawalb@gmail.com>
2025-05-16 23:38:49 +0100

    day 136 => 1023. Camelcase Matching

---
+++ May/may_16ii.js
@@ -0,0 +1,69 @@
+// 1023. Camelcase Matching
+
+const camelMatch = function (queries, pattern) {
+  function checkCheck(one, two) {
+    const n = one.length;
+    const m = two.length;
+
+    let i = 0,
+      j = 0;
+    while (i < n) {
+      if (j < m && one[i] === two[j]) {
+        i++;
+        j++;
+      } else if (one[i] >= "a" && one[i] <= "z") {
+        i++;
+      } else return false;
+    }
+    return j === m;
+  }
+  return queries.map((temp) => checkCheck(temp, pattern));
+};
+
+console.log(
+  camelMatch(
+    ["FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"],
+    "FB"
+  )
+);
+console.log(
+  camelMatch(
+    ["FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"],
+    "FoBa"
+  )
+);
+console.log(
+  camelMatch(
+    ["FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"],
+    "FoBaT"
+  )
+);
+console.log(
+  camelMatch(["CompetitiveProgramming", "CounterPick", "ControlPanel"], "CooP")
+);
+console.log(
+  camelMatch(
+    [
+      "aksvbjLiknuTzqon",
+      "ksvjLimflkpnTzqn",
+      "mmkasvjLiknTxzqn",
+      "ksvjLiurknTzzqbn",
+      "ksvsjLctikgnTzqn",
+      "knzsvzjLiknTszqn",
+    ],
+    "ksvjLiknTzqn"
+  )
+);
+camelMatch(
+  [
+    "uAxaqlzahfialcezsLfj",
+    "cAqlzyahaslccezssLfj",
+    "AqlezahjarflcezshLfj",
+    "AqlzofahaplcejuzsLfj",
+    "tAqlzahavslcezsLwzfj",
+    "AqlzahalcerrzsLpfonj",
+    "AqlzahalceaczdsosLfj",
+    "eAqlzbxahalcezelsLfj",
+  ],
+  "AqlzahalcezsLfj"
+);
