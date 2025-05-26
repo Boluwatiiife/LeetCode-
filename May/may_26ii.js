631baef5525076af46c88cd8d4d71594fdd6c0c4
Boluwatiiife <tundelawalb@gmail.com>
2025-05-26 07:23:28 +0100

    day 146 => 2396. Strictly Palindromic Number

---
+++ May/may_26ii.js
@@ -0,0 +1,9 @@
+// 2396. Strictly Palindromic Number
+
+isStrictlyPalindromic = function (n) {
+  return false;
+};
+
+console.log(isStrictlyPalindromic(9));
+console.log(isStrictlyPalindromic(4));
+console.log(isStrictlyPalindromic(32));
