ce1cf8aa4d60aa516e1abbd4579c17ef4b724ca4
Boluwatiiife <tundelawalb@gmail.com>
2025-05-26 10:19:38 +0100

    day 146 =>2410. Maximum Matching of Players With Trainers

---
+++ May/may_26iv.js
@@ -0,0 +1,24 @@
+// 2410. Maximum Matching of Players With Trainers
+
+const matchPlayersAndTrainers = function (players, trainers) {
+  players.sort((a, b) => a - b);
+  trainers.sort((a, b) => a - b);
+  let count = 0;
+  let i = 0,
+    j = 0;
+
+  while (i < players.length && j < trainers.length) {
+    if (players[i] <= trainers[j]) {
+      count++;
+      i++;
+      j++;
+    } else {
+      j++;
+    }
+  }
+
+  return count;
+};
+
+console.log(matchPlayersAndTrainers([4, 7, 9], [8, 2, 5, 8]));
+console.log(matchPlayersAndTrainers([1, 1, 1], [10]));
