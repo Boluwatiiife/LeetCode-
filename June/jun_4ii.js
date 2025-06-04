735676aae3fba5584c6441eb55e62d296fc63460
Boluwatiiife <tundelawalb@gmail.com>
2025-06-04 09:12:46 +0100

    day 155 => 540. Single Element in a Sorted Array

---
+++ June/jun_4ii.js
@@ -0,0 +1,19 @@
+// 540. Single Element in a Sorted Array
+
+const singleNonDuplicate = function (nums) {
+  const n = nums.length;
+  let left = 0;
+  let right = n - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (mid % 2 === 1) mid--;
+    if (nums[mid] === nums[mid + 1]) left = mid + 2;
+    else right = mid;
+  }
+  return nums[left];
+};
+
+console.log(singleNonDuplicate([1, 1, 2, 3, 3, 4, 4, 8, 8]));
+console.log(singleNonDuplicate([3, 3, 7, 7, 10, 11, 11]));
+console.log(singleNonDuplicate([1, 1, 2, 3, 3]));
