bc5a995ba511b3af82cfa7cbca2f52d810634d78
Boluwatiiife <tundelawalb@gmail.com>
2025-06-10 15:53:49 +0100

    day 161 =>1894. Find the Student that Will Replace the Chalk

--- June/jun_10ii.js
+++ June/jun_10ii.js
@@ -1,18 +1,24 @@
 // 1894. Find the Student that Will Replace the Chalk
 
 const chalkReplacer = function (chalk, k) {
-  let sum = chalk.reduce((a, b) => a + b, 0);
-  while (k >= sum) {
-    k -= sum;
+  const n = chalk.length;
+  let arr = new Array(n);
+  arr[0] = chalk[0];
+  for (let i = 1; i < n; i++) {
+    arr[i] = chalk[i] + arr[i - 1];
+  }
+  const mod = k % arr[n - 1];
+
+  let left = 0;
+  let right = n - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] > mod) right = mid - 1;
+    else left = mid + 1;
   }
 
-  for (let i = 0; i < chalk.length; i++) {
-    let no = chalk[i];
-    if (no > k) return i;
-    else {
-      k -= no;
-    }
-  }
+  return left;
 };
 
 console.log(chalkReplacer([5, 1, 5], 22));
