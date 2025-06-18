9d5d0896e0f30b8f4c58e4ec86baae063b3f26aa
Boluwatiiife <tundelawalb@gmail.com>
2025-06-18 09:03:08 +0100

    day 168 =>2513. Minimize the Maximum of Two Arrays

--- June/jun_17.js
+++ June/jun_17.js
@@ -1,51 +1,46 @@
 // 2513. Minimize the Maximum of Two Arrays
 
 const minimizeSet = function (divisor1, divisor2, uniqueCnt1, uniqueCnt2) {
-  function lcm(one, two) {
-    let lcmm = Math.max(one, two);
-    const temp = Math.max(one, two);
-    let minn = Math.min(one, two);
-    while (lcmm % minn !== 0) {
-      lcmm += temp;
-    }
-
-    return lcmm;
+  function gcd(m, n) {
+    if (m > n) return gcd(n, m);
+    if (m === 0) return n;
+    return gcd(n % m, m);
   }
+  const g = gcd(divisor1, divisor2);
+  const lcm = (divisor1 * divisor2) / g;
 
   function isPossible(maxNum) {
-    const div1 = Math.floor(maxNum / divisor1);
-    const div2 = Math.floor(maxNum / divisor2);
-    const both = Math.floor(maxNum / lcm(divisor1, divisor2));
-    // let totalAval = div1 - div2 + both;
-    let only1 = maxNum - div1;
-    let only2 = maxNum - div2;
-    // let neither = maxNum - totalAval;
-    // let need1 = uniqueCnt1;
-    // let need2 = uniqueCnt2;
-    const commonFree = maxNum - div1 - div2 + both;
-    return (
-      only1 >= uniqueCnt1 &&
-      only2 >= uniqueCnt2 &&
-      commonFree >= uniqueCnt1 + uniqueCnt2
-    );
+    const notByA = maxNum - Math.floor(maxNum / divisor1);
+    const notByB = maxNum - Math.floor(maxNum / divisor2);
+    const byBoth = Math.floor(maxNum / lcm);
+    const notByEither = notByA + notByB - (maxNum - byBoth);
+    const notByAOnly = notByA - notByEither;
+    const notByBOnly = notByB - notByEither;
+
+    if (notByA < uniqueCnt1) return false;
+    if (notByAOnly >= uniqueCnt1) return notByB >= uniqueCnt2;
+    const borrowed = uniqueCnt1 - notByAOnly;
+    const rmainB = notByB - borrowed;
+    return rmainB >= uniqueCnt2;
   }
 
   let left = 1;
-  let right = 1e18;
-  let ans = -1;
+  let right = Number.MAX_SAFE_INTEGER;
+  // let right = 1e18;
 
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
+  while (left < right) {
+    const mid = Math.floor((right - left) / 2) + left;
     if (isPossible(mid)) {
-      ans = mid;
-      right = mid - 1;
-    } else left = mid + 1;
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
   }
 
-  return [ans, left];
+  return right;
 };
 
-console.log(minimizeSet(2, 7, 1, 3));
-console.log(minimizeSet(3, 5, 2, 1));
-console.log(minimizeSet(2, 4, 8, 2));
+console.log(minimizeSet(2, 7, 1, 3)); //4
+console.log(minimizeSet(3, 5, 2, 1)); //3
+console.log(minimizeSet(2, 4, 8, 2)); //15
 console.log(minimizeSet(9, 4, 8, 3));
