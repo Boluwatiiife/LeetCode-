d43a93f51b121a9d8df6ad3dba1f02d7812ea993
Boluwatiiife <tundelawalb@gmail.com>
2025-06-27 21:38:53 +0100

    day 178 =>3488. Closest Equal Element Queries

--- June/jun_27.js
+++ June/jun_27.js
@@ -49,3 +49,4 @@ console.log(solveQueries([1, 3, 1, 4, 1, 3, 2], [0, 3, 5]));
 console.log(solveQueries([1, 2, 3, 4], [0, 1, 2, 3]));
 console.log(solveQueries([14, 14, 4, 2, 19, 19, 14, 19, 14], [2, 4, 8, 6, 3]));
 console.log(solveQueries([6, 12, 17, 9, 16, 7, 6], [5, 6, 0, 4]));
+console.log(solveQueries([6, 12, 3, 9, 4, 16, 7, 6], [5, 6, 0, 4]));
