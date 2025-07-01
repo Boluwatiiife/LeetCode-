8696c8794b932f35fe50b493d8915557f8aeb104
Boluwatiiife <tundelawalb@gmail.com>
2025-07-01 10:33:48 +0100

    day 182=>second half

---
+++ July/jul_1.js
@@ -0,0 +1,25 @@
+// 205. Isomorphic Strings
+
+const isIsomorphic = function (s, t) {
+  let ss = [];
+  let tt = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (ss.includes(s[i])) {
+      if (tt[ss.indexOf(s[i])] !== t[i]) return false;
+    }
+    if (tt.includes(t[i])) {
+      if (ss[tt.indexOf(t[i])] !== s[i]) return false;
+    }
+    ss.push(s[i]);
+    tt.push(t[i]);
+  }
+  return true;
+};
+
+console.log(isIsomorphic("egg", "add"));
+console.log(isIsomorphic("foo", "bar"));
+console.log(isIsomorphic("ofo", "bar"));
+console.log(isIsomorphic("paper", "title"));
+console.log(isIsomorphic("bbbaaaba", "aaabbbba")); // false
+console.log(isIsomorphic("badc", "baba")); // false
