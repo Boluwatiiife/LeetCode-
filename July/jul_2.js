f06b22997cc40d21390305d6f909035a7cae183d
Boluwatiiife <tundelawalb@gmail.com>
2025-07-02 17:09:15 +0100

    day 182=>263. Ugly Number

--- July/jul_2.js
+++ July/jul_2.js
@@ -1,1 +1,21 @@
-// 1025. Divisor Game
+// 263. Ugly Number
+
+const isUgly = function (n) {
+  if (n <= 0) return false;
+
+  for (const prime of [2, 3, 5]) {
+    while (n % prime === 0) {
+      n /= prime;
+    }
+  }
+  return n === 1;
+};
+
+console.log(isUgly(6));
+console.log(isUgly(1));
+console.log(isUgly(14));
+console.log(isUgly(-2147483648));
+console.log(isUgly(50));
+console.log(isUgly(101));
+console.log(isUgly(937351770));
+console.log(isUgly(307));
