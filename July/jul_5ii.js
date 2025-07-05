7b0442c070e518cb448424765df2a6834c0041de
Boluwatiiife <tundelawalb@gmail.com>
2025-07-05 15:16:40 +0100

    day 186=>561. Array Partition

---
+++ July/jul_5ii.js
@@ -0,0 +1,13 @@
+// 561. Array Partition
+
+const arrayPairSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < nums.length; i += 2) {
+    sum += nums[i];
+  }
+  return sum;
+};
+
+console.log(arrayPairSum([1, 4, 3, 2]));
+console.log(arrayPairSum([6, 2, 6, 5, 1, 2]));
