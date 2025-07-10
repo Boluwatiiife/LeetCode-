a4ce285000f25ae0e736dfa719153c386d3878f9
Boluwatiiife <tundelawalb@gmail.com>
2025-07-10 11:54:56 +0100

    day 191=>3440. Reschedule Meetings for Maximum Free Time II

---
+++ July/jul_10.js
@@ -0,0 +1,41 @@
+// 3440. Reschedule Meetings for Maximum Free Time II
+
+const maxFreeTime = function (eventTime, startTime, endTime) {
+  let spaces = [];
+  let time = [];
+  let use = 0;
+  for (let i = 0; i < startTime.length; i++) {
+    time.push(endTime[i] - startTime[i]);
+    spaces.push(startTime[i] - use);
+    use = endTime[i];
+  }
+  spaces.push(eventTime - endTime[endTime.length - 1]);
+
+  let ans = 0;
+
+  for (let i = 0; i < spaces.length - 1; i++) {
+    let temp = [...spaces];
+    temp[i] = 0;
+    temp[i + 1] = 0;
+    // return [spaces, temp];
+    let sum = spaces[i] + spaces[i + 1];
+    let great = temp.filter((xx) => xx >= time[i]);
+    if (great.length > 0) {
+      sum += time[i];
+    }
+    ans = Math.max(ans, sum);
+  }
+
+  return ans;
+};
+
+console.log(maxFreeTime(5, [1, 3], [2, 5]));
+console.log(maxFreeTime(10, [0, 7, 9], [1, 8, 10]));
+console.log(maxFreeTime(10, [0, 3, 7, 9], [1, 4, 8, 10]));
+console.log(maxFreeTime(5, [0, 1, 2, 3, 4], [1, 2, 3, 4, 5]));
+console.log(maxFreeTime(41, [17, 24], [19, 25])); // 24
+console.log(maxFreeTime(25, [18, 22, 24], [22, 23, 25])); // 18
+console.log(maxFreeTime(10, [0, 3, 7, 9], [1, 4, 8, 10])); // 6
+console.log(maxFreeTime(52, [28, 38], [38, 41])); // 38
+console.log(maxFreeTime(178, [3, 20, 98, 118], [17, 39, 111, 141])); // 81
+console.log(maxFreeTime(82, [15, 44, 45, 47, 75], [40, 45, 47, 60, 82])); // 28
