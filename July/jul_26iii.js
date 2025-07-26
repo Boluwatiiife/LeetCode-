bb9a7e8ed40c1234305e71d2ae614c301c67a22f
Boluwatiiife <tundelawalb@gmail.com>
2025-07-26 21:40:26 +0100

    day 207=>1025. Divisor Game

---
+++ July/jul_26iii.js
@@ -0,0 +1,13 @@
+// 1025. Divisor Game
+
+const divisorGame = function (n) {
+  let count = 0;
+  while (n > 1) {
+    n -= 1;
+    count++;
+  }
+  return count % 2 !== 0;
+};
+
+console.log(divisorGame(2));
+console.log(divisorGame(3));
