574b166e0d7ee622ed1cb1ef7090d4b2c1ee1b54
Boluwatiiife <tundelawalb@gmail.com>
2025-07-30 16:06:30 +0100

    day 211=>2373. Largest Local Values in a Matrix

--- July/jul_30ii.js
+++ July/jul_30ii.js
@@ -56,3 +56,42 @@ console.log(
     [1, 1, 1, 1, 1],
   ])
 );
+
+const largestLocall = function (grid) {
+  const n = grid.length;
+  const ans = [];
+
+  for (let i = 0; i < n - 2; i++) {
+    const row = [];
+    for (let j = 0; j < n - 2; j++) {
+      let max_val = 0;
+      for (let x = i; x < i + 3; x++) {
+        for (let y = j; y < j + 3; y++) {
+          max_val = Math.max(max_val, grid[x][y]);
+        }
+      }
+      row.push(max_val);
+    }
+    ans.push(row);
+  }
+
+  return ans;
+};
+
+console.log(
+  largestLocall([
+    [9, 9, 8, 1],
+    [5, 6, 2, 6],
+    [8, 2, 6, 4],
+    [6, 2, 2, 2],
+  ])
+);
+console.log(
+  largestLocall([
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 2, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+  ])
+);
