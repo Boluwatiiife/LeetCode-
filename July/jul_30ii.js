f36acf151a07ede454f616083718cceb2725bbe6
Boluwatiiife <tundelawalb@gmail.com>
2025-07-30 08:36:26 +0100

    day 211=>2373. Largest Local Values in a Matrix

---
+++ July/jul_30ii.js
@@ -0,0 +1,58 @@
+// 2373. Largest Local Values in a Matrix
+
+const largestLocal = function (grid) {
+  const n = grid.length;
+  const m = (n - 3 + 1) * (n - 3 + 1);
+  let root = Math.sqrt(m);
+  let result = [];
+  let yoo = [];
+
+  let dex = 0;
+  while (grid.length >= 3) {
+    let scope = [];
+    while (dex <= grid[0].length - 3) {
+      const use = dex + 3;
+      for (let i = dex; i < use; i++) {
+        let temp = [];
+        for (let j = 0; j < 3; j++) {
+          temp.push(grid[j][i]);
+        }
+        scope.push(temp);
+        if (scope.length === 3) {
+          let temp = [];
+          for (let arr of scope) {
+            temp.push(...arr);
+          }
+          yoo.push(Math.max(...temp));
+          if (yoo.length === root) {
+            result.push(yoo);
+            yoo = [];
+          }
+          scope = [];
+        }
+      }
+      dex++;
+    }
+    grid.shift();
+    dex = 0;
+  }
+  return result;
+};
+
+console.log(
+  largestLocal([
+    [9, 9, 8, 1],
+    [5, 6, 2, 6],
+    [8, 2, 6, 4],
+    [6, 2, 2, 2],
+  ])
+);
+console.log(
+  largestLocal([
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 2, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+  ])
+);
