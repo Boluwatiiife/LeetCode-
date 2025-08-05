b05e36f64b932f1d8a287ff3c4860802d332671e
Boluwatiiife <tundelawalb@gmail.com>
2025-08-05 21:56:34 +0100

    day 217=>8. String to Integer (atoi)

--- August/aug_5.js
+++ August/aug_5.js
@@ -6,38 +6,28 @@ const myAtoi = function (s) {
     const maxx = 2 ** 31 - 1;
     return Math.max(minn, Math.min(maxx, nnn));
   }
-  const arr = s.split("");
-  let temp = [];
-  let count = 0;
 
-  for (const char of arr) {
-    if (char === "-" || char === "+") count++;
-    if (count > 1) break;
-    // if (temp.includes("-")) break;
-    if (char === "-") temp.push("-");
-    if (char === "+") temp.push("+");
-    if (temp.length > 0 && char === " ") break;
-    if (char === " ") continue;
-    if (!isNaN(char)) temp.push(char);
-    // if ((isNaN(char) && temp.includes("-")) || temp.includes("+")) break;
+  let ans = [];
+
+  for (const char of s) {
     if (isNaN(char) && char !== "-" && char !== "+") break;
+    else if (char === " " && ans.length === 0) continue;
+    else if (char === " " && ans.length > 0) break;
+    else if (char === "-" && ans.length === 0) ans.push("-");
+    else if (char === "+" && ans.length === 0) ans.push("+");
+    else if (isNaN(char) && isNaN(ans[ans.length - 1])) {
+      ans.pop();
+      break;
+    } else if (isNaN(char)) break;
+    else {
+      ans.push(char);
+    }
   }
+  if (ans.length === 0) return 0;
 
-  //   return temp;
-  if (temp[temp.indexOf("-") + 1] === "+") {
-    temp = temp.slice(temp.indexOf("+") + 1, temp.length);
-  }
-  //   return temp;
-  if (temp.indexOf("-") === 0 && temp.length === 1) return 0;
-  if (temp.indexOf("-") !== 0 && temp.includes("-"))
-    temp = temp.slice(0, temp.indexOf("-"));
-  while (isNaN(temp[temp.length - 1])) temp.pop();
-  let ans = Number(temp.join(""));
-
-  //   return temp;
-  return clamp_to_32Bit(ans);
-  return Number(temp);
-  return temp;
+  const temp = ans.join("");
+  const anss = clamp_to_32Bit(Number(temp));
+  return isNaN(anss) ? 0 : anss;
 };
 
 console.log(myAtoi("42"));
@@ -46,5 +36,8 @@ console.log(myAtoi("1337c0d3"));
 console.log(myAtoi("0-1"));
 console.log(myAtoi("-91283472332"));
 console.log(myAtoi("-+12"));
+console.log(myAtoi("+-12"));
 console.log(myAtoi("   +0 123"));
 console.log(myAtoi("21474836++"));
+console.log(myAtoi("words and 987"));
+console.log(myAtoi("+"));
