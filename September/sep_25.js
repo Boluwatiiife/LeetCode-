ea962a7b03868404df208d78161dddca7b9ef7c1
Boluwatiiife <tundelawalb@gmail.com>
2025-09-25 05:29:57 +0100

    day 268=>1313. Decompress Run-Length Encoded List

---
+++ September/sep_25.js
@@ -0,0 +1,17 @@
+// 1313. Decompress Run-Length Encoded List
+
+const decompressRLElist = function (nums) {
+  const n = nums.length;
+  let ans = [];
+
+  for (let i = 0; i < n; i += 2) {
+    const [freq, val] = [nums[i], nums[i + 1]];
+    for (let j = 0; j < freq; j++) ans.push(val);
+  }
+
+  return ans;
+};
+
+console.log(decompressRLElist([1, 2, 3, 4]));
+console.log(decompressRLElist([1, 1, 2, 3]));
+console.log(decompressRLElist([1, 1, 2, 3, 5, 4, 3, 3]));
