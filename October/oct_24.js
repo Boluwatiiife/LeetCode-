ee73b949027a6f5753611f043faaa1b32213516c
Boluwatiiife <tundelawalb@gmail.com>
2025-10-24 20:50:36 +0100

    day 296=>1139. Largest 1-Bordered Square

---
+++ October/oct_24.js
@@ -0,0 +1,20 @@
+// 1139. Largest 1-Bordered Square
+
+const largest1BorderedSquare = function (grid) {
+  const n = grid.length;
+};
+
+console.log(
+  largest1BorderedSquare([
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ])
+);
+console.log(largest1BorderedSquare([[1, 1, 0, 0]]));
+console.log(
+  largest1BorderedSquare([
+    [1, 1, 0, 0],
+    [1, 1, 0, 0],
+  ])
+);
