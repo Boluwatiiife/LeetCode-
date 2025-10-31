0d1d464472be47110fab590f17631d5bdf3c82ae
Boluwatiiife <tundelawalb@gmail.com>
2025-10-31 20:55:04 +0100

    day 303=>3692. Majority Frequency Characters

--- October/oct_30.js
+++ October/oct_30.js
@@ -24,6 +24,7 @@ const majorityFrequencyGroup = function (s) {
 console.log(majorityFrequencyGroup("aaabbbccdddde"));
 console.log(majorityFrequencyGroup("abcd"));
 console.log(majorityFrequencyGroup("pfpfgi"));
+console.log(majorityFrequencyGroup("pfpfgidd"));
 console.log(
   majorityFrequencyGroup(
     "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
