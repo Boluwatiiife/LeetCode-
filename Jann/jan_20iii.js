f2e8859c6beb9d2f11e6d50f229ab0c83cc3910e
Boluwatiiife <tundelawalb@gmail.com>
2026-01-20 06:23:19 +0100

    day 382 =>2778. Sum of Squares of Special Elements

---
+++ Jann/jan_20iii.js
@@ -0,0 +1,15 @@
+// 2778. Sum of Squares of Special Elements
+
+const sumOfSquares = function (nums) {
+  const n = nums.length;
+  let ans = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (n % (i + 1) === 0) ans += nums[i] * nums[i];
+  }
+
+  return ans;
+};
+
+console.log(sumOfSquares([1, 2, 3, 4]));
+console.log(sumOfSquares([2, 7, 1, 19, 18, 3]));
