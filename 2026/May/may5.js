1e6820ed9a9846c191058c1a91aa6e7584dd9287
Boluwatiiife <tundelawalb@gmail.com>
2026-05-05 19:23:21 +0100

    all tests work

---
+++ 2026/May/may5.js
@@ -0,0 +1,12 @@
+// 1903. Largest Odd Number in String
+
+const largestOddNumber = function (num) {
+  for (let i = num.length - 1; i >= 0; i--) {
+    if (Number(num[i]) % 2 !== 0) return num.substring(0, i + 1);
+  }
+  return "";
+};
+
+console.log(largestOddNumber("52"));
+console.log(largestOddNumber("4206"));
+console.log(largestOddNumber("35427"));
