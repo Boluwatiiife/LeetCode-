9b23b080f6913066c26aac07e565de51dedede1d
Boluwatiiife <tundelawalb@gmail.com>
2026-08-05 21:45:18 +0100

    day 574: picking up GoLang

---
+++ 2026/GoLangg/aug8.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	ans := []int{}
+	for k, v := range nums {
+		for key, val := range nums {
+			if v+val == target && k != key {
+				ans = append(ans, k, key)
+				return ans
+			}
+		}
+	}
+	return ans
+}
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
+	fmt.Println(twoSum([]int{3, 3}, 6))
+	fmt.Println(twoSum([]int{1, 2, 3}, 9))
+}
+
+// func main() {
+// 	twoSum([]int{2, 7, 11, 15}, 9)
+// 	// fmt.Println()
+// }
