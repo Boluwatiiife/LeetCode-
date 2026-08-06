b9fc27000cbbd29ec2b671604bfc0f1aa8f2d77a
Boluwatiiife <tundelawalb@gmail.com>
2026-08-06 21:55:48 +0100

    day 575: picking up GoLang

---
+++ 2026/GoLangg/aug6.go
@@ -0,0 +1,31 @@
+// 3345. Smallest Divisible Digit Product I
+package main
+
+import (
+	"fmt"
+)
+
+func smallestNumber(n int, t int) int {
+
+	for x := n; ; x++ {
+		if digitProduct(x)%t == 0 {
+			return x
+		}
+	}
+}
+func digitProduct(x int) int {
+	product := 1
+	for {
+		product *= x % 10
+		x /= 10
+		if x == 0 {
+			break
+		}
+	}
+	return product
+}
+
+func main() {
+	fmt.Println(smallestNumber(10, 2))
+	fmt.Println(smallestNumber(15, 3))
+}
