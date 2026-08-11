e22f18d507e69d6328cc15d7f0b307a9a67cbf9b
Boluwatiiife <tundelawalb@gmail.com>
2026-08-11 19:54:02 +0100

    leetcode with GoLang

---
+++ 2026/GoLangg/aug11.go
@@ -0,0 +1,33 @@
+// 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum
+
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func missingInteger(nums []int) int {
+	sum := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1]+1 == nums[i] {
+			sum += nums[i]
+		} else {
+			break
+		}
+	}
+	for {
+		if slices.Contains(nums, sum) {
+			sum++
+		} else {
+			break
+		}
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(missingInteger([]int{1, 2, 3, 2, 5}))
+	fmt.Println(missingInteger([]int{3, 4, 5, 1, 12, 14, 13}))
+}
